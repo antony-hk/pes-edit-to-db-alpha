684e5347ec8f7890abe80d0834ffc22760fe7b2a
Antony <antsgit@gmail.com>
2022-12-16 20:38:27 +0000

    Rename some files for adding types

--- spec/pes2021/edit/Tactics.mjs
+++ spec/pes2021/edit/Tactics.mjs
@@ -1,5 +1,5 @@
 // Same as PES 2020
-import * as EditTactics2020 from '../../pes2020/edit/Tactics.mjs';
+import * as EditTactics2020 from '../../pes2020/edit/Tactics.ts';
 
 export const isFullyCovered = EditTactics2020.isFullyCovered;
 export const recordLength = EditTactics2020.recordLength;
